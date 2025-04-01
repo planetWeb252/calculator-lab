b7f66e7d8f2a1e846e4b9626a507906f138262e9
planetWeb252 <juanjosefrancojimenez@outlook.com>
2025-04-01 15:45:27 +0200

    add class Sum and Division

---
+++ src/main/java/Division.java
@@ -0,0 +1,13 @@
+public class Division {
+    public static double divisionDouble(double num1, double num2){
+        return num1/num2;
+    }
+
+    public static int divisionInt (int num1,int num2){
+        return num1/num2;
+    }
+
+    public static double inversor(double num1){
+        return 1/num1;
+    }
+}
